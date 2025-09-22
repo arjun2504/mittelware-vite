58c99a190f5bb081b64e75288d83340ea5b36930
Arjun <arjun2504@gmail.com>
2025-09-22 14:46:03 +0530

    fix: persist session

--- src/services/supabase/client.ts
+++ src/services/supabase/client.ts
@@ -3,12 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 
-const isLocalhost = location.hostname === "localhost" || location.hostname === "127.0.0.1";
-
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: true,
-    persistSession: isLocalhost,
+    persistSession: true,
     detectSessionInUrl: true,
   }
 });
